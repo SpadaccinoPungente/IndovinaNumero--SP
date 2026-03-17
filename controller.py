e270884cd7d91f093e36b2a01588ac39b8850390
Giuseppe Averta <g.averta3@gmail.com>
2026-03-17 15:11:23 +0100

    soluzione svolta in aula

--- controller.py
+++ controller.py
@@ -5,3 +5,67 @@ class Controller(object):
     def __init__(self, view: View):
         self._view = view
         self._model = Model()
+
+    def reset(self, e):
+        self._model.reset() # resetto lo stato del gioco lato modello!
+        self._view._txtT.value = self._model.T
+        self._view._lvOut.controls.clear()
+        self._view._lvOut.controls.append(
+            ft.Text("Inizia il gioco! Indovina a numero sto pensando.")
+        )
+        self._view.update()
+
+
+    def play(self, e):
+        tentativoStr = self._view._txtInTentativo.value
+        try:
+            tentativo = int(tentativoStr)
+        except ValueError:
+            self._view._lvOut.controls.append(
+                ft.Text("Errore, devi inserire un valore numerico"))
+            self._view.update()
+            return
+
+        res = self._model.play(tentativo)
+
+        if res == 0:
+            """Ho vinto!"""
+            self._view._lvOut.controls.append(
+                ft.Text(f"Hai vinto! il valore corretto era: {tentativo}",
+                        color="green")
+            )
+            self._view.update()
+            return
+        elif res == 2:
+            """Non ho più vite."""
+            self._view._lvOut.controls.append(
+                ft.Text(f"Hai perso! il valore corretto era {self._model.segreto}.")
+            )
+            self._view.update()
+            return
+        elif res == -1:
+            """Allora il segreto è più piccolo del tentativo."""
+            self._view._lvOut.controls.append(
+                ft.Text(f"Ritenta! il segreto è più piccolo di {tentativo}")
+            )
+            self._view.update()
+            return
+        else:
+            """Allora il segreto è più grande di tentativo."""
+            self._view._lvOut.controls.append(
+                ft.Text(f"Ritenta! il segreto è più grande di {tentativo}")
+            )
+            self._view.update()
+            return
+
+
+
+
+
+
+
+    def getNmax(self):
+        return self._model.Nmax
+
+    def getTmax(self):
+        return self._model.Tmax
--- view.py
+++ view.py
@@ -12,6 +12,33 @@ class View(object):
         self._titolo = ft.Text("Indovina il numero",
                                color="blue", size=24)
 
+        self._txtNmax = ft.TextField(label = "Numero Max",
+                                     value=self._controller.getNmax(),
+                                     disabled = True
+                                     )
+
+        self._txtTmax = ft.TextField(label = "Num tentativi max",
+                                     value = self._controller.getTmax(),
+                                     disabled = True
+                                     )
+
+        self._txtT = ft.TextField(label="Tentativi rimanenti",
+                                    disabled = True)
+
+        self._row1 = ft.Row(controls = [self._txtNmax, self._txtTmax, self._txtT])
+
+        self._txtInTentativo = ft.TextField(label = "Valore")
+        self._btnReset = ft.ElevatedButton(text = "Nuova Partita",
+                                           on_click = self._controller.reset)
+        self._btnPlay = ft.ElevatedButton(text = "Indovina",
+                                           on_click = self._controller.play)
+
+        self._row2 = ft.Row(controls = [self._txtInTentativo,
+                                        self._btnReset,
+                                        self._btnPlay])
+
+        self._lvOut = ft.ListView(expand=True)
+        self._page.add(self._row1, self._row2, self._lvOut)
         self._page.update()
 
     def setController(self,controller):
